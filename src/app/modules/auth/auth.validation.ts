9daffb752cce9e89ed284914cc555349a0574f7c
Rahad-Ullah <rahadullah10@gmail.com>
2025-09-23 16:51:56 +0600

    modify: auth schemas

--- src/app/modules/auth/auth.validation.ts
+++ src/app/modules/auth/auth.validation.ts
@@ -14,6 +14,7 @@ const createLoginZodSchema = z.object({
   body: z.object({
     email: z
       .string({ required_error: 'Email is required' })
+      .email('Invalid email format')
       .nonempty('Email cannot be empty'),
     password: z
       .string({ required_error: 'Password is required' })
@@ -23,28 +24,43 @@ const createLoginZodSchema = z.object({
 
 const createForgetPasswordZodSchema = z.object({
   body: z.object({
-    email: z.string({ required_error: 'Email is required' }),
+    email: z
+      .string({ required_error: 'Email is required' })
+      .email('Invalid email format')
+      .nonempty('Email is required'),
   }),
 });
 
 const createResetPasswordZodSchema = z.object({
   body: z.object({
-    newPassword: z.string({ required_error: 'Password is required' }),
-    confirmPassword: z.string({
-      required_error: 'Confirm Password is required',
-    }),
+    newPassword: z
+      .string({ required_error: 'Password is required' })
+      .nonempty('Password cannot be empty')
+      .min(8, 'Password must be at least 8 characters long'),
+    confirmPassword: z
+      .string({
+        required_error: 'Confirm Password is required',
+      })
+      .nonempty('Confirm Password cannot be empty')
+      .min(8, 'Confirm Password must be at least 8 characters long'),
   }),
 });
 
 const createChangePasswordZodSchema = z.object({
   body: z.object({
-    currentPassword: z.string({
-      required_error: 'Current Password is required',
-    }),
-    newPassword: z.string({ required_error: 'New Password is required' }),
-    confirmPassword: z.string({
-      required_error: 'Confirm Password is required',
-    }),
+    currentPassword: z
+      .string({
+        required_error: 'Current Password is required',
+      })
+      .nonempty('Current Password cannot be empty'),
+    newPassword: z
+      .string({ required_error: 'New Password is required' })
+      .nonempty('New Password cannot be empty'),
+    confirmPassword: z
+      .string({
+        required_error: 'Confirm Password is required',
+      })
+      .nonempty('Confirm Password cannot be empty'),
   }),
 });
 
