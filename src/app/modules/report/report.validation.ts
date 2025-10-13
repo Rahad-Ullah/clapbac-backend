735a0d182fdf40c6bcb4791db257a64abae98697
Rahad-Ullah <rahadullah10@gmail.com>
2025-10-13 13:16:35 +0600

    feat: defined zod validations for updating report

--- src/app/modules/report/report.validation.ts
+++ src/app/modules/report/report.validation.ts
@@ -1,18 +1,31 @@
 import { z } from 'zod';
+import { ReportStatus } from './report.constants';
 
 // create report validation
 const createReportZodSchema = z.object({
-    body: z
-        .object({
-            review: z
-                .string({ required_error: 'Review ID is required' })
-                .nonempty('Review ID cannot be empty')
-                .length(24, 'Review ID must be a valid ObjectId'),
-            reason: z
-                .string({ required_error: 'Reason is required' })
-                .nonempty('Reason cannot be empty'),
-        })
-        .strict(),
+  body: z
+    .object({
+      review: z
+        .string({ required_error: 'Review ID is required' })
+        .nonempty('Review ID cannot be empty')
+        .length(24, 'Review ID must be a valid ObjectId'),
+      reason: z
+        .string({ required_error: 'Reason is required' })
+        .nonempty('Reason cannot be empty'),
+    })
+    .strict(),
 });
 
-export const ReportValidations = { createReportZodSchema };
+// update report validation
+const updateReportZodSchema = z.object({
+  body: z
+    .object({
+      status: z.nativeEnum(ReportStatus).optional(),
+    })
+    .strict(),
+});
+
+export const ReportValidations = {
+  createReportZodSchema,
+  updateReportZodSchema,
+};
