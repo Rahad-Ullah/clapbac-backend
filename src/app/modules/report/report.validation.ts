88ff3178c1fb02942c6c28c168369a69f4024d9e
Rahad-Ullah <rahadullah10@gmail.com>
2025-10-13 13:13:33 +0600

    feat: defined zod validations for creating report

--- src/app/modules/report/report.validation.ts
+++ src/app/modules/report/report.validation.ts
@@ -1,3 +1,18 @@
 import { z } from 'zod';
 
-export const ReportValidations = {  };
+// create report validation
+const createReportZodSchema = z.object({
+    body: z
+        .object({
+            review: z
+                .string({ required_error: 'Review ID is required' })
+                .nonempty('Review ID cannot be empty')
+                .length(24, 'Review ID must be a valid ObjectId'),
+            reason: z
+                .string({ required_error: 'Reason is required' })
+                .nonempty('Reason cannot be empty'),
+        })
+        .strict(),
+});
+
+export const ReportValidations = { createReportZodSchema };
