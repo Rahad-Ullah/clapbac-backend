8806b0444ab2324d6c105553a908e910b95f9887
Rahad Ullah <rahadullah10@gmail.com>
2026-02-21 11:38:11 +0600

    refactor(review): min review rating is 0.5

--- src/app/modules/review/review.model.ts
+++ src/app/modules/review/review.model.ts
@@ -7,14 +7,14 @@ const reviewSchema = new Schema<IReview, ReviewModel>(
     company: { type: Schema.Types.ObjectId, ref: 'Company', required: true },
     reviewerName: { type: String, required: true },
     reviewerAddress: { type: String, required: true, default: '' },
-    reviewRating: { type: Number, required: true, min: 1, max: 5 },
+    reviewRating: { type: Number, required: true, min: 0, max: 5 },
     reviewMessage: { type: String, required: true },
     reviewSource: { type: String, required: true },
     sourceLink: { type: String, required: true },
     experienceDate: { type: Date, required: true },
     clapbacTitle: { type: String, required: true },
     clapbacMessage: { type: String, required: true },
-    clapbacRating: { type: Number, required: true, min: 1, max: 5 },
+    clapbacRating: { type: Number, required: true, min: 0, max: 5 },
     reviewerType: { type: String, required: true },
     reviewerConsequence: { type: String, required: true },
     helpfulCount: { type: Number, default: 0 },
@@ -22,7 +22,7 @@ const reviewSchema = new Schema<IReview, ReviewModel>(
   },
   {
     timestamps: true,
-  }
+  },
 );
 
 export const Review = model<IReview, ReviewModel>('Review', reviewSchema);
